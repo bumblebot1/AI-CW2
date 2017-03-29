139f67dd86855ff09c3a5e8a0bba60ad8989f95e
bumblebot1 <rm14834@my.bristol.ac.uk>
2017-03-30 00:25:09 +0100

    done part 2

--- assignment2_wp_12862.pl
+++ assignment2_wp_12862.pl
@@ -1,4 +1,28 @@
 % Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
 % find_identity(-A)
 find_identity(A):-
-  A='Not yet implemented'.
+  findall(A, actor(A), ActorNames),
+  create_actor_data(ActorNames, [], Actors),
+  search_actors(Actors, Actor),
+  Actor = actor_data(A, _),
+  !.
+
+search_actors([Actor], Actor).
+search_actors(Unfiltered, Actor) :-
+  agent_ask_oracle(oscar, o(1), link, Link),
+  filter_actors(Unfiltered, Link, [], Filtered),
+  search_actors(Filtered, Actor).
+
+create_actor_data([], Actors, Actors).
+create_actor_data([Curr|Rest], Processed, Actors) :-
+  wp(Curr, WT),
+  findall(L, wt_link(WT, L), Links),
+  create_actor_data(Rest,[actor_data(Curr,Links)|Processed],Actors).
+
+filter_actors([], _, Filtered, Filtered).
+filter_actors([Actor|Unfiltered], Link, Processed, Filtered) :-
+  Actor = actor_data(_, Links),
+  (
+  memberchk(Link, Links) -> filter_actors(Unfiltered,Link,[Actor|Processed],Filtered);
+  otherwise -> filter_actors(Unfiltered,Link,Processed,Filtered)
+  ).
