97b8dc3ac7afdc0956bbbf628cd79c3573e7690e
bumblebot1 <rm14834@my.bristol.ac.uk>
2017-04-14 02:12:41 +0300

    improved my wp code and made it usable for part 3

--- assignment2_wp_12862.pl
+++ assignment2_wp_12862.pl
@@ -1,28 +1,29 @@
 % Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
 % find_identity(-A)
-find_identity(A):-
-  findall(A, actor(A), ActorNames),
-  create_actor_data(ActorNames, [], Actors),
-  search_actors(Actors, Actor),
-  Actor = actor_data(A, _),
-  !.
+:-dynamic eliminated_actor/1.
 
-search_actors([Actor], Actor).
-search_actors(Unfiltered, Actor) :-
-  agent_ask_oracle(oscar, o(1), link, Link),
-  filter_actors(Unfiltered, Link, [], Filtered),
-  search_actors(Filtered, Actor).
 
-create_actor_data([], Actors, Actors).
-create_actor_data([Curr|Rest], Processed, Actors) :-
-  wp(Curr, WT),
-  findall(L, wt_link(WT, L), Links),
-  create_actor_data(Rest,[actor_data(Curr,Links)|Processed],Actors).
-
-filter_actors([], _, Filtered, Filtered).
-filter_actors([Actor|Unfiltered], Link, Processed, Filtered) :-
-  Actor = actor_data(_, Links),
+find_identity(A) :-  
+  findall(N, possible_actor(N), Ns),
+  length(Ns, Len),
   (
-  memberchk(Link, Links) -> filter_actors(Unfiltered,Link,[Actor|Processed],Filtered);
-  otherwise -> filter_actors(Unfiltered,Link,Processed,Filtered)
+    Len = 1 -> possible_actor(A);
+    otherwise -> query_oracle(o(1)), find_identity(A)
+  ),!,
+  retractall(eliminated_actor(_)).
+
+query_oracle(Oracle) :-
+  agent_ask_oracle(oscar, Oracle, link, RandomLink),
+  findall(N, possible_actor(N), Ns),
+  maplist(get_wiki(RandomLink), Ns).
+
+get_wiki(RandomLink, CurrActor):-
+  findall(Link, (wp(CurrActor,WT), wt_link(WT,Link)), ListOfLinks),
+  (
+    \+ memberchk(RandomLink, ListOfLinks) -> assert(eliminated_actor(CurrActor));
+    otherwise -> true
   ).
+
+possible_actor(A) :-
+  actor(A),
+  \+ eliminated_actor(A).
